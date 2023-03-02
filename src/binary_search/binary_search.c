94fb1b5131adfd28d613ddd65a958e8c4ed9a4fb
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-03-02 17:15:10 -0300

    Problem description

---
+++ src/binary_search/binary_search.c
@@ -0,0 +1,8 @@
+// Problem:
+// Given a sorted array arr[] of n elements, write a function to
+// search a given element x in arr[] and return the index of x in the array.
+// Consider array is 0 base index.
+
+// Input: arr[] = { 10, 20, 30, 40, 100, 200, 500, 100000 } target = 30
+// Output: 2
+
